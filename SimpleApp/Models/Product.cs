a018297ffa671a5dbe8f4c10c4a775722042814e
TemoElizbarashvili <temoelizbarashvili23@gmail.com>
2023-04-30 17:18:21 +0400

    Make Product model & Home View w Controller
    make Product model with name and price and 1 static method which returns array of products. Also add HomeController and Home view folder with index.cshtml

---
+++ SimpleApp/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product Kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+            Product LifeJacket = new Product
+            {
+                Name = "LifeJacket",
+                Price = 48.95M
+            };
+            return new Product[] { Kayak, LifeJacket };
+        }
+    }
+}
